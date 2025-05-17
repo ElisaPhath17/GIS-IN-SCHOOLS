716e1b0ba0e8d068e3ceabdc73b7fbb8fa29e7a0
ElisaPhath17 <ngwpha002@myuct.ac.za>
2025-05-17 03:47:21 +0200

    Add files via upload

---
+++ data/PointsOfFocus_7.js
@@ -0,0 +1,26 @@
+var json_PointsOfFocus_7 = {
+"type": "FeatureCollection",
+"name": "PointsOfFocus_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "WardNo.": 74.0, "WardPop": 17668.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.36930963105366, -34.000798610936414 ] } },
+{ "type": "Feature", "properties": { "id": "2", "WardNo.": 57.0, "WardPop": 17589.0, "No.Schools": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 18.459289496311463, -33.941698535233442 ] } },
+{ "type": "Feature", "properties": { "id": "3", "WardNo.": 58.0, "WardPop": 17425.0, "No.Schools": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 18.485145779431509, -33.961497060593935 ] } },
+{ "type": "Feature", "properties": { "id": "4", "WardNo.": 59.0, "WardPop": 17238.0, "No.Schools": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 18.467120256342103, -33.969918821381611 ] } },
+{ "type": "Feature", "properties": { "id": "5", "WardNo.": 49.0, "WardPop": 17234.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.513661565958188, -33.957212305105472 ] } },
+{ "type": "Feature", "properties": { "id": "6", "WardNo.": 80.0, "WardPop": 17223.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.546757608351843, -34.034633404276356 ] } },
+{ "type": "Feature", "properties": { "id": "7", "WardNo.": 33.0, "WardPop": 17037.0, "No.Schools": 1.0 }, "geometry": { "type": "Point", "coordinates": [ 18.570988639390059, -34.016607881186957 ] } },
+{ "type": "Feature", "properties": { "id": "8", "WardNo.": 78.0, "WardPop": 17793.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.600686427430794, -34.048078671498786 ] } },
+{ "type": "Feature", "properties": { "id": "9", "WardNo.": 76.0, "WardPop": 17659.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.61738219881688, -34.025472892542396 ] } },
+{ "type": "Feature", "properties": { "id": "10", "WardNo.": 87.0, "WardPop": 17139.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.651512492535343, -34.012618626077007 ] } },
+{ "type": "Feature", "properties": { "id": "12", "WardNo.": 16.0, "WardPop": 17106.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.685495036064545, -34.01808538307953 ] } },
+{ "type": "Feature", "properties": { "id": "13", "WardNo.": 11.0, "WardPop": 17551.0, "No.Schools": 2.0 }, "geometry": { "type": "Point", "coordinates": [ 18.675004772627265, -33.934015525392063 ] } },
+{ "type": "Feature", "properties": { "id": "14", "WardNo.": 3.0, "WardPop": 17704.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.659786503133756, -33.889542718425581 ] } },
+{ "type": "Feature", "properties": { "id": "15", "WardNo.": 103.0, "WardPop": 17535.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.687415788524891, -33.830442642722623 ] } },
+{ "type": "Feature", "properties": { "id": "16", "WardNo.": 105.0, "WardPop": 17282.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.662002755972615, -33.738542025004513 ] } },
+{ "type": "Feature", "properties": { "id": "17", "WardNo.": 4.0, "WardPop": 17084.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.512184064065607, -33.847286164297962 ] } },
+{ "type": "Feature", "properties": { "id": "18", "WardNo.": 107.0, "WardPop": 17321.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.504205553845708, -33.809462115848071 ] } },
+{ "type": "Feature", "properties": { "id": "19", "WardNo.": 43.0, "WardPop": 17358.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.577489647717378, -34.070979950833681 ] } },
+{ "type": "Feature", "properties": { "id": "11", "WardNo.": 19.0, "WardPop": 16984.0, "No.Schools": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 18.663597202701691, -33.97054805827581 ] } }
+]
+}
